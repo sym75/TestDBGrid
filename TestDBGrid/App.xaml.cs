bc3b605905a8570f0f0810608f7823d1545c4f07
Asus <Asus@ASUS-PC97>
2021-10-27 21:18:31 +0300

    Добавьте файлы проекта.

---
+++ TestDBGrid/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DevExpress.XamarinForms.DataGrid;
+
+namespace TestDBGrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            DevExpress.XamarinForms.DataGrid.Initializer.Init();
+            InitializeComponent();
+            DataGridView grid = new DataGridView();
+            MainPage = new ContentPage //MainPage();
+            {
+                Content = grid,
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
